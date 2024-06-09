df0d805c3e2d60ccbce4522c65853bc8a49cd0e6
Nouamane El Gueddari <nouamane@schorcht-it.de>
2024-06-09 11:09:51 +0200

    first commit

---
+++ models/recommendation_model.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from utils.text_preprocessing import preprocess_text  # Ensure this import is correct
+
+class RecommendationModel:
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.df = pd.read_csv(data_path)
+        self.tfidf_matrix = None
+        self.cosine_sim = None
+        print(self.df['cuisines'].isnull().sum())
+        self._prepare_model()
+
+
+    def _prepare_model(self):
+        self.df = self.df.dropna(subset=['cuisines'])
+        print(self.df['cuisines'].isnull().sum())
+        self.df['processed_cuisines'] = self.df['cuisines'].apply(preprocess_text)
+        vectorizer = TfidfVectorizer()
+        self.tfidf_matrix = vectorizer.fit_transform(self.df['processed_cuisines'])
+        self.cosine_sim = cosine_similarity(self.tfidf_matrix, self.tfidf_matrix)
+
+    def get_recommendations(self, index, num_recommendations=10):
+        sim_scores = list(enumerate(self.cosine_sim[index]))
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = sim_scores[1:num_recommendations+1]
+        restaurant_indices = [i[0] for i in sim_scores]
+        return self.df[['name', 'cuisines']].iloc[restaurant_indices]
---
+++ utils/text_preprocessing.py
@@ -0,0 +1,33 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+try:  
+    nltk.data.find('omw-1.4')
+except LookupError:
+    nltk.download('omw-1.4') 
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    nltk.download('wordnet')
+
+
+def preprocess_text(text):
+    text = text.lower() 
+    tokens = word_tokenize(text)
+    tokens = [word for word in tokens if word.isalnum()]
+    tokens = [word for word in tokens if word not in stopwords.words('english')] ## remove stopwords 
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    return ' '.join(tokens)
